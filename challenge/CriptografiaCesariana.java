add23f2f29e49260ee58524a6c747d96de07fc26
Valeria Melo <59574875+val-bit@users.noreply.github.com>
2020-04-23 02:15:15 -0300

    Update CriptografiaCesariana.java

--- challenge/CriptografiaCesariana.java
+++ challenge/CriptografiaCesariana.java
@@ -12,55 +12,54 @@ public class CriptografiaCesariana implements Criptografia {
     public String descriptografar(String letra) {
         return descriptografeOuCriptografe(letra, int = -3));
     }
-}
-private String descriptografeOuCriptografe(String letra,int valor) {
 
-}
-    String alfabeto = "abcdefghijklmnopqrstuvwxyz";
-    char charLetra = letra.charAt(0);
-    int codigo = charLetra;
-    codigo = codigo + valor;
+public String criptografar(String letra, int numero) {
 
-    for (int i = 0; i < letra.length(); i++) {
-        charLetra = letra.charAt(i);
-        if (codigo.isLetter(valor)) {
-            antigaPosicao = alfabeto.indexOf(digito);
-            novaPosicao = antigaPosicao + numero;
-            if (novaPosicao < 0) {
-                novaPosicao = novaPosicao + 26; }
-            if (novaPosicao > 25) {
-                novaPosicao = novaPosicao - 26;
-    return "" +(char) codigo;
-
-public interface Criptografia {
-
-    public String criptografar(String letra);
-    String alfabeto = "abcdefghijklmnopqrstuvwxyz";
-    String textoNovo = "";
-    Character digito;
-    int antigaPosicao;
-    int novaPosicao;
-
-    for (int i = 0; i < letra.length(); i++) {
-        digito = letra.charAt(i);
-        if (Character.isLetter(digito)) {
-            antigaPosicao = alfabeto.indexOf(digito);
-            novaPosicao = antigaPosicao + numero;
-            if (novaPosicao < 0) {
-                novaPosicao = novaPosicao + 26; }
-            if (novaPosicao > 25) {
-                novaPosicao = novaPosicao - 26;
-            }textoNovo = textoNovo.concat(String.valueOf(alfabeto.charAt(novaPosicao)));
-        }else {
-            textoNovo = textoNovo.concat(String.valueOf(digito)); }
-    }
+        String alfabeto = "abcdefghijklmnopqrstuvwxyz";
+        String textoNovo = "";
+        Character digito;
+        int antigaPosicao;
+        int novaPosicao;
+    
+        for (int i = 0; i < letra.length(); i++) {
+            digito = letra.charAt(i);
+            if (Character.isLetter(digito)) {
+                antigaPosicao = alfabeto.indexOf(digito);
+                novaPosicao = antigaPosicao + numero;
+                if (novaPosicao < 0) {
+                    novaPosicao = novaPosicao + 26; }
+                if (novaPosicao > 25) {
+                    novaPosicao = novaPosicao - 26;
+                }textoNovo = textoNovo.concat(String.valueOf(alfabeto.charAt(novaPosicao)));
+            }else {
+                textoNovo = textoNovo.concat(String.valueOf(digito)); }
+        }
         return textoNovo;
+    }
 
+public String Descriptografar(String letra, int numero) {
 
-}
-
-}
-
+        String alfabeto = "abcdefghijklmnopqrstuvwxyz";
+        String textoNovo = "";
+        Character digito;
+        int antigaPosicao;
+        int novaPosicao;
+    
+        for (int i = 0; i < letra.length(); i++) {
+            digito = letra.charAt(i);
+            if (Character.isLetter(digito)) {
+                antigaPosicao = alfabeto.indexOf(digito);
+                novaPosicao = antigaPosicao - numero;
+                if (novaPosicao < 0) {
+                    novaPosicao = novaPosicao + 26; }
+                if (novaPosicao > 25) {
+                    novaPosicao = novaPosicao - 26;
+                }textoNovo = textoNovo.concat(String.valueOf(alfabeto.charAt(novaPosicao)));
+            }else {
+                textoNovo = textoNovo.concat(String.valueOf(digito)); }
+        }
+        return textoNovo;
+    }
 
 
 
